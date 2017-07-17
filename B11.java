c12694f35e7425755a985d8a1a5b67ee151f0f6b
Elakiyaaa <elakthamizh@gmail.com>
2017-07-17 16:52:47 +0530

    Add files via upload

---
+++ B11.java
@@ -0,0 +1,16 @@
+package GUVI;
+
+import java.util.Scanner;
+
+public class B11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter the number to be powered:");
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=(int) Math.pow(a,2);
+System.out.println(b);
+	}
+
+}
